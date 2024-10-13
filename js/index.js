86fd600ec2375a73b70b036a1902161cc8bb923c
fforlani <federico.forlani@hotmail.com>
2024-10-13 13:29:00 +0200

    invio mail di contatto + modifiche varie

--- js/index.js
+++ js/index.js
@@ -1,19 +1,37 @@
-(function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+// sending email
+var data = {
+  service_id: 'service_96kijqs',
+  template_id: 'template_2ew50ac',
+  user_id: '9lC9BARc7nT00q2fL',
+  template_params: {}
+};
+
+function sendEmail() {
+  let contactForm = $('#contactForm')[0];
+  if (!contactForm.checkValidity()) {
+    contactForm.classList.add('was-validated');
+    return;
+  }
+  data.template_params = Object.fromEntries(new FormData(contactForm));
+  $('#sendMailSpinner').show();
+  $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+    type: 'POST',
+    data: JSON.stringify(data),
+    contentType: 'application/json'
+  }).done(function() {
+    $('#sendMailSpinner').hide();
+    $('#mailSuccess').show();
+  }).fail(function(error) {
+    $('#sendMailSpinner').hide();
+    $('#mailError').show();
+  });
+}
+
+
+function showSpinner(){
+  $('#spinnerDiv').show();
+}
+
+function hideSpinner(){
+  $('#spinnerDiv').hide();
+}
